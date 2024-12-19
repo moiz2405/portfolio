82c47983cfb77c9ee68652410f5fe6a2770efdcf
Moiz2405 <km856307@gmail.com>
2024-12-19 19:42:07 +0530

    added pokemon sounds

--- components/StaticPokemon.tsx
+++ components/StaticPokemon.tsx
@@ -1,26 +1,31 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 const pokemonGifs = [
-  '/images/pokemon/pikachu.gif',
-  '/images/pokemon/charmander.gif',
-  '/images/pokemon/squirtle.gif',
-  '/images/pokemon/bulbasaur.gif',
-]
+  { gif: '/images/pokemon/pikachu.gif', sound: '/sounds/pika.mp3' },
+  { gif: '/images/pokemon/charmander.gif', sound: '/sounds/char.mp3' },
+  { gif: '/images/pokemon/squirtle.gif', sound: '/sounds/squir.mp3' },
+  { gif: '/images/pokemon/bulbasaur.gif', sound: '/sounds/bulba.mp3' },
+];
 
 export default function StaticPokemon() {
+  const handleHover = (sound) => {
+    const audio = new Audio(sound);
+    audio.play();
+  };
+
   return (
     <div className="flex justify-center space-x-2 my-4">
-      {pokemonGifs.map((gif, index) => (
+      {pokemonGifs.map((pokemon, index) => (
         <Image
           key={index}
-          src={gif}
+          src={pokemon.gif}
           alt={`Pokemon ${index + 1}`}
           width={40}
           height={40}
-          className="pixelated"
+          className="pixelated cursor-pointer"
+          onMouseEnter={() => handleHover(pokemon.sound)}
         />
       ))}
     </div>
-  )
+  );
 }
-
